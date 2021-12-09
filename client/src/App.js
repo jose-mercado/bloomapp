4eac2e9c6112ae604a233380c9318e950fcb24de
jose-mercado <josefmercado99@gmail.com>
2021-12-08 18:20:04 -0800

    monolith

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
-
+import {useState, useEffect} from 'react'
 import './App.css';
 
+function url(path){
+  return process.env.NODE_ENV === "development" ? `http://localhost:1234${path}` : path
+}
+
 function App() {
+  const [data, setData] = useState("hi")
+  useEffect(()=>{
+    fetch(url("/api/"))
+    .then(res=>res.json())
+    .then(apiData =>{
+      setData(apiData.data)
+    })
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
-        Hello? 
+        API Data: {data}
       </header>
     </div>
   );
--- index.js
+++ index.js
@@ -2,20 +2,24 @@ const dotenv = require("dotenv").config()
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 9000;
+const path = require("path");
 const cors = require("cors");
 
 app.use(cors());
+app.use(express.static(path.join(__dirname, "client/build")));
 app.use(express.json());
 
 app.use("/api/", (_, res)=>{
-    res.json({data: "This is API DATA!!!"});
+    res.json({"message": "success", "people": [{"craft": "ISS", "name": "Mark Vande Hei"}, {"craft": "ISS", "name": "Pyotr Dubrov"}, {"craft": "ISS", "name": "Anton Shkaplerov"}, {"craft": "Shenzhou 13", "name": "Zhai Zhigang"}, {"craft": "Shenzhou 13", "name": "Wang Yaping"}, {"craft": "Shenzhou 13", "name": "Ye Guangfu"}, {"craft": "ISS", "name": "Raja Chari"}, {"craft": "ISS", "name": "Tom Marshburn"}, {"craft": "ISS", "name": "Kayla Barron"}, {"craft": "ISS", "name": "Matthias Maurer"}, {"craft": "ISS", "name": "Alexander Misurkin"}, {"craft": "ISS", "name": "Yusaku Maezawa"}, {"craft": "ISS", "name": "Yozo Hirano"}], "number": 13});
 });
 
 app.listen(port,()=>{
     console.log(`server is running on ${port}`)
 });
 
-
+app.use("*", (_,res)=>{
+    res.sendFile(path.join(__dirname, "client/build", "index.html"))
+});
 // *** SOME CONSOLE LOGS TO TEST IF WORKING ***
 
 // console.log("its alive");
