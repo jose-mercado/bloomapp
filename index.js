3a188671df461f1644d8d574ac7359be8a92694d
jose-mercado <josefmercado99@gmail.com>
2021-12-08 17:31:16 -0800

    API push

---
+++ index.js
@@ -0,0 +1,28 @@
+const dotenv = require("dotenv").config()
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 9000;
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/", (_, res)=>{
+    res.json({data: "This is API DATA!!!"});
+})
+
+app.listen(port,()=>{
+    console.log(`server is running on ${port}`)
+})
+
+
+
+// *** SOME CONSOLE LOGS TO TEST IF WORKING ***
+
+// console.log("its alive");
+// console.log(__dirname);
+// console.log(__filename);
+// console.log(process.env.USERNAME);
+// console.log(process.env.APIKEY);
+// console.log(process.env.DBPASS);
+// console.log(process.env.PORT);
